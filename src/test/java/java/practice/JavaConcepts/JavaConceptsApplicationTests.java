4ee56d83d0e76d62242b463efbbc78311c8ee15d
Mahesh <m.konduri@outlook.com>
2020-01-18 15:11:10 -0500

    cod initial commit

---
+++ src/test/java/java/practice/JavaConcepts/JavaConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package java.practice.JavaConcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
